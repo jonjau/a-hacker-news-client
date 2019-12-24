250146dfedd64b2a26c11552f751823cd43a4507
jonjau <jonjau3Q@gmail.com>
2019-12-24 12:53:02 +0700

    search bar

--- src/App.js
+++ src/App.js
@@ -20,16 +20,24 @@ const list = [
   },
 ];
 
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      list: list,
+      list,
+      searchTerm: '',
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -38,10 +46,22 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        {this.state.list.map( item =>
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
